58b29ac526ac79b3b28488b7677ce503d3402a89
akamomer <akam@akameng.com>
2023-02-16 23:56:13 +0300

    Create sendsms.py

---
+++ sendsms.py
@@ -0,0 +1,20 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+
+url = "https://sms.standingtech.com/api/v3/sms/send"
+
+headers = CaseInsensitiveDict()
+headers["Authorization"] = "Bearer 49|LNFe8WJ7CPtvl2mzowAB4ll4enbFR0XGgnQh2qWY"
+headers["Content-Type"] = "application/json"
+headers["Accept"] = "application/json"
+
+data = """
+{"recipient":"9647xxxxxxxxx","sender_id":"SenderID","type":"plain","message":"This is a test message"
+}
+"""
+
+
+resp = requests.post(url, headers=headers, data=data)
+
+print(resp.status_code)
+
