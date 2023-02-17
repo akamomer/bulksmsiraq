1fc0d7f5e7d3b8890aa31eaa7da3f1430e74a9ed
akamomer <akam@akameng.com>
2023-02-17 14:36:33 +0300

    Create sendsms-guzzle.php

---
+++ sendsms-guzzle.php
@@ -0,0 +1,35 @@
+<?php
+require "guzzle/vendor/autoload.php";
+use GuzzleHttp\Client;
+
+$client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'https://sms.standingtech.com/api/v3/sms/',
+]);
+$now = date("Y-m-d H:i:s");
+$body = <<<DATA
+{"recipient":"9647xxxxxxxxx",
+"sender_id":"Standing",
+"type":"plain",
+"message":"This is a test message using Guzzle at $now"
+}
+DATA;
+
+$response = $client->request('POST', 'send', [
+'headers' => [
+    'Authorization'     => 'Bearer API_KEY',
+    'Content-Type'     	=> 'application/json',
+    'Accept'     		=> 'application/json',
+    ],
+//'stream' => true,
+'body' => $body,
+//'sink' => 'temp.txt',
+//'verify' => false,
+
+]);
+
+$response = $response->getBody()->getContents();
+
+echo "<pre>";
+print_r(json_decode($response, true));
+
