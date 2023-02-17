bbf356e6626cfb046f96f5e25d3256a6c2e652f6
akamomer <akam@akameng.com>
2023-02-17 15:03:11 +0300

    Create sendsms.cs

---
+++ sendsms.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Net.Http;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer API_KEY");
+var data = @"{
+""recipient"":""9647xxxxxxxxx"",
+""sender_id"":""SENDERID"",
+""type"":""plain"",
+""message"":""This is test using C# to send SMS""
+}";
+
+var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+var response = client.PostAsync("https://sms.standingtech.com/api/v3/sms/send", content).Result;
+var responseContent = response.Content.ReadAsStringAsync().Result;
+Console.WriteLine(responseContent);
+    }
+}
