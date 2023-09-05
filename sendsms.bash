91068c94960044ce3a2a011aa5c3333938d84c96
akamomer <akam@akameng.com>
2023-09-05 12:48:24 +0300

    Update sendsms.bash

--- sendsms.bash
+++ sendsms.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -X POST https://sms.standingtech.com/api/v3/sms/send -H "Authorization: Bearer 49|LNFe8WJ7CPtvl2mzowAB4ll4enbFR0XGgnQh2qWY" -H "Content-Type: application/json" -H "Accept: application/json" --data-binary @- <<DATA
+curl -X POST https://gateway.standingtech.com/api/v3/sms/send -H "Authorization: Bearer 49|LNFe8WJ7CPtvl2mzowAB4ll4enbFR0XGgnQh2qWY" -H "Content-Type: application/json" -H "Accept: application/json" --data-binary @- <<DATA
 {"recipient":"9647xxxxxxxxx","sender_id":"SenderID","type":"plain","message":"This is a test message"
 }
 DATA
